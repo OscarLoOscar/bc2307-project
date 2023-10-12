da01a4e587d6e88a7e2d0596b63621bd7ea1d4cc
OscarLoOscar <lokongkitoscar@gmail.com>
2023-10-12 19:31:13 +0800

    updated

--- stock/src/main.js
+++ stock/src/main.js
@@ -1,43 +1,41 @@
-import { createApp } from 'vue';
-import App from './components/App.vue';
-import ElementPlus from 'element-plus';
-import 'element-plus/lib/theme-chalk/index.css';
+import { createApp } from "vue";
+import App from "./components/App.vue";
+import "./components/assets/tailwind.css";
+// import ElementPlus from 'element-plus'
+import ElementPlus from "element-plus";
+import "element-plus/theme-chalk/index.css"; // Import Element UI CSS
 
-import { library } from '@fortawesome/fontawesome-svg-core';
+// fontawesome
+import { library } from "@fortawesome/fontawesome-svg-core";
 import {
   faCaretUp,
   faCaretDown,
   faSearch,
-  faCheck,
-  faTimes,
-  faPlus,
-} from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-
+  faCheck, faTimes, faPlus
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 library.add(faCaretUp, faCaretDown, faSearch, faCheck, faTimes, faPlus);
 
-const app = createApp(App);
+// app.use(ElementPlus)
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).use(ElementPlus).mount("#app");
 
-app.use(ElementPlus);
-app.component('font-awesome-icon', FontAwesomeIcon);
-
-// Custom filters
+// custom filter
 app.config.globalProperties.$filters = {
   // percent
   price_negative(value) {
-    if (value.toString().includes('-')) {
+    if (value.toString().includes("-")) {
       return value;
     }
-    return '';
+    return "";
   },
   // comma
   comma_separator(value) {
-    // Add commas for dollar format, e.g., 1,000,000
-    return value.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',');
+    // Add comma for dollar format, i.e. 1,000,000
+    return value.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
   },
   // truncate string
   string_trunc(value, size) {
-    if (!value) return '';
+    if (!value) return "";
     value = value.toString();
     if (value.length <= size) {
       return value;
@@ -45,6 +43,3 @@ app.config.globalProperties.$filters = {
     return value.substr(0, size);
   },
 };
-
-// Mount the app to the DOM
-app.mount('#app');
