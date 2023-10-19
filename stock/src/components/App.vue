d55356d881b794a847b733403045b2b5b577e3f9
OscarLoOscar <lokongkitoscar@gmail.com>
2023-10-19 15:26:03 +0800

    updated

--- stock/src/components/App.vue
+++ stock/src/components/App.vue
@@ -4,8 +4,8 @@
     <div class="order-book-container">
       <div>
         <div class="order-form">
-
-        <div class="custom-dropdown">
+      
+          <div class="custom-dropdown">
           <el-row class="button-header">Stock Symbol : </el-row>
           <el-row>
             <el-select v-model="stockSymbol" class="custom-dropdown">
@@ -99,10 +99,7 @@
       </div>
       </div>
 
-  <div> 
-    <div id="kline-chart" style="width: 600px; height: 600px;" ref="klineChart"></div>
-    </div>
-</div>
+      <klineChart /></div>
 
 
     </div>
@@ -113,9 +110,14 @@ import { ref, onMounted } from 'vue';
 import axios from 'axios';
 import * as echarts from 'echarts';
 import 'echarts/theme/macarons'; // Optional: Add a theme for your chart
+import klineChart from './model/klineChart.vue';
+ // Import the KlineChart component
 
 export default {
   name: 'App',
+  components: {
+    klineChart,
+  },
   setup() {
       const    price_input= ref(1000.0);
       const   quantity_input= ref(100);
@@ -141,13 +143,13 @@ export default {
     const sellOrders = ref([]);
 
     const retrieveQueue = async () => {
-
+const symbol = stockSymbol.value;
       try {
         const responseBuy = await axios.get(
-          "http://localhost:8085/transactions/bidQueue?stockId=TSLA"
+          `http://localhost:8085/transactions/bidQueue?stockId=${symbol}`
         );
         const responseAsk = await axios.get(
-          "http://localhost:8085/transactions/askQueue?stockId=TSLA"
+          `http://localhost:8085/transactions/askQueue?stockId=${symbol}`
         );
         sellOrders.value = responseAsk.data;
         buyOrders.value = responseBuy.data;
@@ -193,279 +195,10 @@ export default {
       console.error('Error placing order:', error);
     });
 };
-
-const klineChart = ref(null); // Reference to the Kline chart div
-
-    const initKlineChart = () => {
-      // Initialize ECharts instance in the kline-chart div
-      const myChart = echarts.init(klineChart.value);
-    // Define Finnhub client and API key
-// const apiKey = "cju3it9r01qr958213c0cju3it9r01qr958213cg"; // Replace with your Finnhub API key
-const symbol = stockSymbol.value;
-//stockSymbol.value;
-const time = "D";//1,5,15,60,D,M,W
-const from = "2022-11-01";
-const to = "2023-10-01";
-
-// const apiUrl = `https://finnhub.io/api/v1/stock/candle?symbol=${symbol}&resolution=${time}&from=${from}&to=${to}&token=${apiKey}`;
-const apiUrl = `http://localhost:8085/api/v1/getCandleData?symbol=${symbol}&resolution=${time}&from=${from}&to=${to}`;
-
-console.log(apiUrl);
-
-function fetchData(){
-fetch(apiUrl)
-.then(response=>{
-  if(!response.ok){
-    throw new Error('Network response was not ok');
-  }
-  return response.json();
-})
-  .then(data => {
-    // Handle the data here
-    console.log(data);
-
-    // Extract data
-    var categoryData = data.t.map(timestamp => new Date(timestamp * 1000).toLocaleDateString()); // Convert timestamps to date strings
-    var values = data.c.map((close, index) => [data.o[index], close, data.l[index], data.h[index]]);
-    var vols = data.v;
-
-    var ma10 = calculateMA(10, data);
-    var ma50 = calculateMA(50, data);
-    var ma100 = calculateMA(100, data);
-    var ma250 = calculateMA(250, data);
-
-    // ECharts option
-    var option = {
-      tooltip: {
-        trigger: 'axis',
-        axisPointer: {
-          type: 'cross'
-        }
-      },
-      grid: [{
-        left: '3%',
-        top: '1%',
-        height: '58%'
-      }, {
-        left: '3%',
-        right: '10%',
-        top: '65%',
-        height: '10%'
-      }, {
-        left: '3%',
-        right: '10%',
-        top: '78%',
-        height: '10%'
-      }],
-      xAxis: [{
-        type: 'category',
-        data: categoryData,
-        scale: true,
-        boundaryGap: false,
-        axisLine: {
-          onZero: false,
-          lineStyle: {
-            color: 'red',
-          }
-        },
-        splitLine: {
-          show: false
-        },
-        splitNumber: 20
-      }, {
-        type: 'category',
-        gridIndex: 1,
-        data: categoryData,
-        axisLabel: {
-          show: false
-        },
-      }, {
-        type: 'category',
-        gridIndex: 2,
-        data: categoryData,
-        axisLabel: {
-          show: false
-        },
-      }],
-      yAxis: [{
-        scale: true,
-        splitArea: {
-          show: true
-        },
-        axisLine: {
-          lineStyle: {
-            color: 'red',
-          }
-        },
-        position: 'right'
-      }, {
-        gridIndex: 1,
-        splitNumber: 3,
-        axisLine: {
-          onZero: false,
-          lineStyle: {
-            color: 'red'
-          }
-        },
-        axisTick: {
-          show: false
-        },
-        splitLine: {
-          show: false
-        },
-        axisLabel: {
-          show: true
-        },
-        position: 'right'
-      }, {
-        gridIndex: 2,
-        splitNumber: 4,
-        axisLine: {
-          onZero: false,
-          lineStyle: {
-            color: 'red'
-          }
-        },
-        axisTick: {
-          show: false
-        },
-        splitLine: {
-          show: false
-        },
-        axisLabel: {
-          show: true
-        },
-        position: 'right'
-      }],
-      dataZoom: [{
-        type: 'inside',
-        start: 100,
-        end: 80
-      }, {
-        show: true,
-        type: 'slider',
-        y: '90%',
-        xAxisIndex: [0, 1],
-        start: 50,
-        end: 100
-      }, {
-        show: false,
-        xAxisIndex: [0, 2],
-        type: 'slider',
-        start: 20,
-        end: 100
-      }],
-      series: [{
-        name: 'Candlestick',
-        type: 'candlestick',
-        data: values,
-        markPoint: {
-          data: [{
-            name: 'XX标点'
-          }]
-        },
-        markLine: {
-          silent: true,
-          data: [{
-            yAxis: 2222,
-          }]
-        }
-      }, {
-        name: 'MA10',
-        type: 'line',
-        data: ma10,
-        smooth: true,
-        lineStyle: {
-          normal: {
-            opacity: 1
-          }
-        }
-      }, {
-        name: 'MA50',
-        type: 'line',
-        data: ma50,
-        smooth: true,
-        lineStyle: {
-          normal: {
-            opacity: 2
-          }
-        }
-      }, {
-        name: 'MA100',
-        type: 'line',
-        data: ma100,
-        smooth: true,
-        lineStyle: {
-          normal: {
-            opacity: 3
-          }
-        }
-      }, {
-        name: 'MA250',
-        type: 'line',
-        data: ma250,
-        smooth: true,
-        lineStyle: {
-          normal: {
-            opacity: 4
-          }
-        }
-      }, {
-        name: 'Volume',
-        type: 'bar',
-        xAxisIndex: 1,
-        yAxisIndex: 1,
-        data: vols,
-        itemStyle: {
-          normal: {
-            color: function (params) {
-              var colorList;
-              if (values[params.dataIndex][1] > values[params.dataIndex][0]) {
-                colorList = '#ef232a';
-              } else {
-                colorList = '#14b143';
-              }
-              return colorList;
-            },
-          }
-        }
-      }]
-    };
-
-    // Set the option and render the chart
-    myChart.setOption(option);
-  })
-  .catch(error => {
-    console.error('Error fetching data:', error);
-  });
-}
-
-// Set up a timer to call fetchData every, for example, 5 seconds (5000 milliseconds)
-const interval = 100000; // 100 seconds
-setInterval(fetchData, interval);
-
-// Define calculateMA function
-function calculateMA(dayCount, data) {
-  var result = [];
-  for (var i = 0, len = data.c.length; i < len; i++) {
-    if (i < dayCount) {
-      result.push('-');
-      continue;
-    }
-    var sum = 0;
-    for (var j = 0; j < dayCount; j++) {
-      sum += data.c[i - j];
-    }
-    result.push((sum / dayCount).toFixed(2)); // Rounded to 2 decimal places
-  }
-  return result;
-}
-    };
-
     // Fetch data initially
     onMounted(() => {
       retrieveQueue();
-      initKlineChart();
+      // initKlineChart();
     });
 
     // Fetch data periodically every 2 seconds
@@ -547,10 +280,14 @@ body::before {
 
 .order-book-buy {
   width: 47%;
+  margin-left: 30px;
+
 }
 
 .order-book-sell {
   width: 47%;
+  margin-left: -40px;
+
 }
 
 /* Styling for the order form and panel */
@@ -560,7 +297,7 @@ body::before {
   flex-direction: column;
   align-items: left;
   /* Center content horizontally */
-  margin-right: 50px;
+  margin-left: 30px;
   background-color: #f7f7f7;
   /* Light gray background */
   border: 2px solid #b2bfe0;
@@ -568,6 +305,7 @@ body::before {
   padding: 20px;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
   /* Box shadow for depth */
+
 }
 
 .order-panel {
